72cc6e079cfc867b6b910456452167cec68721f4
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-11-20 14:27:30 -0500

    Strip `-fstack-protector-strong` flag
    Valgrind adds the flag `-fno-stack-protector`, which almost certainly
    clashes with `-fstack-protector-strong`. Expect this is causing at least
    some of our build woes. So try stripping this flag to see if we can get
    this to build without it.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,10 @@
 
 set -e -o pipefail
 
+# Valgrind sets `-fno-stack-protector`.
+export CFLAGS="$(echo "${CFLAGS}" | sed 's/-fstack-protector-strong//g')"
+export CXXFLAGS="$(echo "${CXXFLAGS}" | sed 's/-fstack-protector-strong//g')"
+
 ./configure --prefix=${PREFIX} --disable-dependency-tracking --enable-only64bit
 
 make
