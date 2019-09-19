22ca9f053909efc0189d0e75195942b98a62409c
Julian Rüth <julian.rueth@fsfe.org>
2019-09-19 16:35:17 +0200

    Fix autodetected Darwin version

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e -o pipefail
+set -exo pipefail
 
 # Valgrind sets `-fno-stack-protector`.
 export CFLAGS="$(echo "${CFLAGS}" | sed 's/-fstack-protector-strong//g')"
@@ -11,11 +11,18 @@ if [ "$(uname)" == "Darwin" ]
 then
     MACH_INC="/usr/include/mach/"
     sed -i.bak -e \
-        "s@${MACH_INC}@${CONDA_BUILD_SYSROOT}/${MACH_INC}@g" \
+        "s@${MACH_INC}@${CONDA_BUILD_SYSROOT}${MACH_INC}@g" \
         "${SRC_DIR}/coregrind/Makefile.in"
 fi
 
 ./configure --prefix=${PREFIX} --disable-dependency-tracking --enable-only64bit
+
+if [ "$(uname)" == "Darwin" ]
+then
+    # Valgrind's configure.ac parses uname -r to find out the Darwin version.
+    # However, that might not be the one for which we are building.
+    echo "#define DARWIN_VERS DARWIN_${MACOSX_DEPLOYMENT_TARGET/./_}" >> config.h
+fi
 
 make
 #make check
