61a968af89b7096727d9fa95c9dbba0652237ae2
Julian Rüth <julian.rueth@fsfe.org>
2019-09-19 21:15:01 +0200

    Replace sed with patching Makefile.am directly

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,17 +6,7 @@ set -exo pipefail
 export CFLAGS="$(echo "${CFLAGS}" | sed 's/-fstack-protector-strong//g')"
 export CXXFLAGS="$(echo "${CXXFLAGS}" | sed 's/-fstack-protector-strong//g')"
 
-# Patch Valgrind to use the macOS SDK for mach headers.
-if [ "$(uname)" == "Darwin" ]
-then
-    MACH_INC="/usr/include/mach/"
-    sed -i.bak -e \
-        "s@${MACH_INC}@${CONDA_BUILD_SYSROOT}${MACH_INC}@g" \
-        "${SRC_DIR}/coregrind/Makefile.in"
-fi
-
 ./autogen.sh
-
 ./configure --prefix=${PREFIX} --disable-dependency-tracking --enable-only64bit
 
 make
